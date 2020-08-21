fdb917c8219ed8a5ebc40e92308565ad42fe528e
Jeremy Muriel <jeremy@jeremm.fr>
2020-08-21 15:27:42 +0200

    add go test for new junos_security_utm_policy

---
+++ junos/resource_security_utm_policy_test.go
@@ -0,0 +1,73 @@
+package junos_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccJunosSecurityUtmPolicy_basic(t *testing.T) {
+	if os.Getenv("TESTACC_SWITCH") == "" {
+		resource.Test(t, resource.TestCase{
+			PreCheck:  func() { testAccPreCheck(t) },
+			Providers: testAccProviders,
+			Steps: []resource.TestStep{
+				{
+					Config: testAccJunosSecurityUtmPolicyConfigCreate(),
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"anti_virus.#", "1"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"anti_virus.0.http_profile", "junos-av-defaults"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"traffic_sessions_per_client.#", "1"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"traffic_sessions_per_client.0.over_limit", "log-and-permit"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"web_filtering_profile", "junos-wf-local-default"),
+					),
+				},
+				{
+					Config: testAccJunosSecurityUtmPolicyConfigUpdate(),
+					Check: resource.ComposeTestCheckFunc(
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"anti_virus.#", "0"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"traffic_sessions_per_client.#", "0"),
+						resource.TestCheckResourceAttr("junos_security_utm_policy.testacc_Policy",
+							"web_filtering_profile", "junos-wf-enhanced-default"),
+					),
+				},
+				{
+					ResourceName:      "junos_security_utm_policy.testacc_Policy",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
+			},
+		})
+	}
+}
+
+func testAccJunosSecurityUtmPolicyConfigCreate() string {
+	return `
+resource junos_security_utm_policy "testacc_Policy" {
+  name  = "testacc Policy"
+  anti_virus {
+    http_profile = "junos-av-defaults"
+  }
+  traffic_sessions_per_client {
+    over_limit = "log-and-permit"
+  }
+  web_filtering_profile = "junos-wf-local-default"
+}
+`
+}
+func testAccJunosSecurityUtmPolicyConfigUpdate() string {
+	return `
+resource junos_security_utm_policy "testacc_Policy" {
+  name  = "testacc Policy"
+  web_filtering_profile = "junos-wf-enhanced-default"
+}
+`
+}
