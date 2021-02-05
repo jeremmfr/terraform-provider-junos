2a7ee2f4c01738a52fe0fdc1e45462bbdf273ff5
a-d-v <44755022+a-d-v@users.noreply.github.com>
2021-02-05 15:32:52 +0100

    Thank you, good catch
    Co-authored-by: Jeremy Muriel <jeremy@jeremm.fr>

--- junos/resource_security_utm_custom_url_category_test.go
+++ junos/resource_security_utm_custom_url_category_test.go
@@ -45,9 +45,15 @@ func TestAccJunosSecurityUtmCustomURLCategory_basic(t *testing.T) {
 
 func testAccJunosSecurityUtmCustomURLCategoryConfigCreate() string {
 	return `
+resource junos_security_utm_custom_url_pattern "testacc_URLCategory1" {
+  name  = "custom-pattern1"
+  value = ["*.google.com"]
+}
 resource junos_security_utm_custom_url_category "testacc_URLCategory" {
-  name  = "testacc_URLCategory"
-  value = ["custom-pattern1"]
+  name = "testacc_URLCategory"
+  value = [
+    junos_security_utm_custom_url_pattern.testacc_URLCategory1.name,
+  ]
 }
 `
 }
