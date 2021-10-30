e47a1d1e6e9582f3d887bdb1180f259091826778
Jeremy Muriel <jeremy@jeremm.fr>
2021-10-30 15:58:54 +0200

    r/interface_logical: add dhcp and dhcpv6_client block arguments
    inside family_inet and family_inet6 block arguments
    Fixes parts of #301

--- junos/resource_interface_logical_test.go
+++ junos/resource_interface_logical_test.go
@@ -182,6 +182,15 @@ func TestAccJunosInterfaceLogical_basic(t *testing.T) {
 					ImportState:       true,
 					ImportStateVerify: true,
 				},
+				{
+					Config: testAccJunosInterfaceLogicalConfigUpdate2(testaccInterface),
+				},
+				{
+					Config: testAccJunosInterfaceLogicalConfigUpdate3(testaccInterface),
+				},
+				{
+					Config: testAccJunosInterfaceLogicalConfigUpdate4(testaccInterface),
+				},
 			},
 		})
 	}
@@ -323,6 +332,9 @@ resource junos_interface_physical testacc_interface_logical_phy {
   vlan_tagging = true
 }
 resource junos_interface_logical testacc_interface_logical {
+  lifecycle {
+    create_before_destroy = true
+  }
   name                       = "${junos_interface_physical.testacc_interface_logical_phy.name}.100"
   vlan_id                    = 101
   description                = "testacc_interface_${junos_interface_physical.testacc_interface_logical_phy.name}.100"
@@ -376,6 +388,117 @@ resource junos_interface_logical testacc_interface_logical {
       cidr_ip = "fe80::1/64"
     }
   }
+}
+`)
+}
+
+func testAccJunosInterfaceLogicalConfigUpdate2(interFace string) string {
+	return fmt.Sprintf(`
+resource junos_interface_physical testacc_interface_logical_phy {
+  name         = "` + interFace + `"
+  vlan_tagging = true
+}
+resource junos_interface_logical testacc_interface_logical {
+  name    = "${junos_interface_physical.testacc_interface_logical_phy.name}.100"
+  vlan_id = 100
+  family_inet {
+    dhcp {}
+  }
+  family_inet6 {
+    dhcpv6_client {
+      client_type                 = "stateful"
+      client_identifier_duid_type = "vendor"
+      client_ia_type_na           = true
+    }
+  }
+}
+`)
+}
+
+func testAccJunosInterfaceLogicalConfigUpdate3(interFace string) string {
+	return fmt.Sprintf(`
+resource junos_interface_physical testacc_interface_logical_phy {
+  name         = "` + interFace + `"
+  vlan_tagging = true
+}
+resource junos_interface_logical testacc_interface_logical {
+  name    = "${junos_interface_physical.testacc_interface_logical_phy.name}.100"
+  vlan_id = 100
+  family_inet {
+    dhcp {
+      client_identifier_ascii                        = "BBAA#1"
+      client_identifier_prefix_hostname              = true
+      client_identifier_prefix_routing_instance_name = true
+      client_identifier_use_interface_description    = "device"
+      client_identifier_userid_ascii                 = "BBCC#2"
+      force_discover                                 = true
+      lease_time                                     = 600
+      metric                                         = 0
+      no_dns_install                                 = true
+      options_no_hostname                            = true
+      retransmission_attempt                         = 0
+      retransmission_interval                        = 4
+      server_address                                 = "192.0.2.1"
+      update_server                                  = true
+      vendor_id                                      = 2
+    }
+  }
+  family_inet6 {
+    dhcpv6_client {
+      client_type                               = "stateful"
+      client_identifier_duid_type               = "vendor"
+      client_ia_type_na                         = true
+      client_ia_type_pd                         = true
+      no_dns_install                            = true
+      prefix_delegating_preferred_prefix_length = 0
+      prefix_delegating_sub_prefix_length       = 5
+      rapid_commit                              = true
+      req_option                                = ["fqdn"]
+      retransmission_attempt                    = 0
+      update_router_advertisement_interface = [
+        junos_interface_logical.testacc_interface_logical2.name,
+      ]
+      update_server = true
+    }
+  }
+}
+resource junos_interface_logical testacc_interface_logical2 {
+  name = "${junos_interface_physical.testacc_interface_logical_phy.name}.101"
+}
+`)
+}
+
+func testAccJunosInterfaceLogicalConfigUpdate4(interFace string) string {
+	return fmt.Sprintf(`
+resource junos_interface_physical testacc_interface_logical_phy {
+  name         = "` + interFace + `"
+  vlan_tagging = true
+}
+resource junos_interface_logical testacc_interface_logical {
+  name    = "${junos_interface_physical.testacc_interface_logical_phy.name}.100"
+  vlan_id = 100
+  family_inet {
+    dhcp {
+      client_identifier_hexadecimal        = "11BBee"
+      client_identifier_userid_hexadecimal = "00AAff"
+      lease_time_infinite                  = true
+    }
+  }
+  family_inet6 {
+    dhcpv6_client {
+      client_type                 = "stateful"
+      client_identifier_duid_type = "vendor"
+      client_ia_type_pd           = true
+      req_option                  = ["fqdn", "domain"]
+      update_router_advertisement_interface = [
+        junos_interface_logical.testacc_interface_logical2.name,
+        "${junos_interface_physical.testacc_interface_logical_phy.name}.100",
+      ]
+    }
+  }
+}
+resource junos_interface_logical testacc_interface_logical2 {
+  name = "${junos_interface_physical.testacc_interface_logical_phy.name}.101"
 }
 `)
 }
