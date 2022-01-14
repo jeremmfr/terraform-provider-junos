5886cc05e909986f871097acdc0ab561a2d802d1
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-14 17:34:44 +0100

    r/eventoptions_policy: add workaround
    when fake update/delete are enabled

--- junos/resource_eventoptions_policy.go
+++ junos/resource_eventoptions_policy.go
@@ -553,6 +553,17 @@ func resourceEventoptionsPolicyReadWJnprSess(
 func resourceEventoptionsPolicyUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delEventoptionsPolicy(d.Get("name").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setEventoptionsPolicy(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -584,6 +595,13 @@ func resourceEventoptionsPolicyUpdate(ctx context.Context, d *schema.ResourceDat
 
 func resourceEventoptionsPolicyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delEventoptionsPolicy(d.Get("name").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
