97d24a0256ffa2201b911c39c6864c9bf9610929
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-14 17:38:26 +0100

    r/firewall_filter: add workaround
    when fake update/delete are enabled

--- junos/resource_firewall_filter.go
+++ junos/resource_firewall_filter.go
@@ -355,6 +355,17 @@ func resourceFirewallFilterReadWJnprSess(
 func resourceFirewallFilterUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delFirewallFilter(d.Get("name").(string), d.Get("family").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setFirewallFilter(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -386,6 +397,13 @@ func resourceFirewallFilterUpdate(ctx context.Context, d *schema.ResourceData, m
 
 func resourceFirewallFilterDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delFirewallFilter(d.Get("name").(string), d.Get("family").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
