c5d87cd6c9a3c4ceb9d2d076ee97142ce48aefbb
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-14 17:36:43 +0100

    r/evpn: add workaround
    when fake update/delete are enabled

--- junos/resource_evpn.go
+++ junos/resource_evpn.go
@@ -199,6 +199,17 @@ func resourceEvpnReadWJnprSess(d *schema.ResourceData, m interface{}, jnprSess *
 func resourceEvpnUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delEvpn(false, d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setEvpn(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -230,6 +241,13 @@ func resourceEvpnUpdate(ctx context.Context, d *schema.ResourceData, m interface
 
 func resourceEvpnDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delEvpn(true, d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
