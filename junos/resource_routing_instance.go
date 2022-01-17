c2c6a94783000ce00c6c69f465e47f10cb527e04
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-17 17:48:14 +0100

    r/routing_instance: add workaround
    when fake update/delete are enabled

--- junos/resource_routing_instance.go
+++ junos/resource_routing_instance.go
@@ -226,6 +226,17 @@ func resourceRoutingInstanceReadWJnprSess(
 func resourceRoutingInstanceUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delRoutingInstanceOpts(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setRoutingInstance(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -257,6 +268,13 @@ func resourceRoutingInstanceUpdate(ctx context.Context, d *schema.ResourceData, 
 
 func resourceRoutingInstanceDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delRoutingInstance(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
