3c464bc919db3e370c11300806cb0ba710d276ca
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-17 18:44:21 +0100

    r/security_ike_gateway: add workaround
    when fake update/delete are enabled

--- junos/resource_security_ike_gateway.go
+++ junos/resource_security_ike_gateway.go
@@ -362,6 +362,17 @@ func resourceIkeGatewayReadWJnprSess(d *schema.ResourceData, m interface{}, jnpr
 func resourceIkeGatewayUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delIkeGateway(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setIkeGateway(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -393,6 +404,13 @@ func resourceIkeGatewayUpdate(ctx context.Context, d *schema.ResourceData, m int
 
 func resourceIkeGatewayDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delIkeGateway(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
