dcb71105bfedd55a9bef426d683989de0c5696c4
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-17 18:45:15 +0100

    r/security_ike_policy: add workaround
    when fake update/delete are enabled

--- junos/resource_security_ike_policy.go
+++ junos/resource_security_ike_policy.go
@@ -171,6 +171,17 @@ func resourceIkePolicyReadWJnprSess(d *schema.ResourceData, m interface{}, jnprS
 func resourceIkePolicyUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delIkePolicy(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setIkePolicy(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -203,6 +214,13 @@ func resourceIkePolicyUpdate(ctx context.Context, d *schema.ResourceData, m inte
 
 func resourceIkePolicyDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delIkePolicy(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
