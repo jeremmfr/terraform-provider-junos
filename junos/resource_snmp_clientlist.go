3099b1e2e555ac402a9f77784f6ac6aed6ead877
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-20 09:21:59 +0100

    r/snmp_clientlist: add workaround
    when fake update/delete are enabled

--- junos/resource_snmp_clientlist.go
+++ junos/resource_snmp_clientlist.go
@@ -124,6 +124,17 @@ func resourceSnmpClientlistReadWJnprSess(
 func resourceSnmpClientlistUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delSnmpClientlist(d.Get("name").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setSnmpClientlist(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -155,6 +166,13 @@ func resourceSnmpClientlistUpdate(ctx context.Context, d *schema.ResourceData, m
 
 func resourceSnmpClientlistDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delSnmpClientlist(d.Get("name").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
