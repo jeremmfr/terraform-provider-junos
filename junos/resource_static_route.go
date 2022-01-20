67cacf97ae97e85e47af58f57d5dc0f985d1997b
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-20 09:23:34 +0100

    r/static_route: add workaround
    when fake update/delete are enabled

--- junos/resource_static_route.go
+++ junos/resource_static_route.go
@@ -308,6 +308,17 @@ func resourceStaticRouteReadWJnprSess(d *schema.ResourceData, m interface{}, jnp
 func resourceStaticRouteUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delStaticRoute(d.Get("destination").(string), d.Get("routing_instance").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setStaticRoute(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -340,6 +351,13 @@ func resourceStaticRouteUpdate(ctx context.Context, d *schema.ResourceData, m in
 
 func resourceStaticRouteDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	sess := m.(*Session)
+	if sess.junosFakeDeleteAlso {
+		if err := delStaticRoute(d.Get("destination").(string), d.Get("routing_instance").(string), m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
