7a6f66af8635ea53dba89d6893d9cad93fccdb19
Jeremy Muriel <jeremy@jeremm.fr>
2022-01-20 09:21:26 +0100

    r/snmp: add workaround
    when fake update/delete are enabled

--- junos/resource_snmp.go
+++ junos/resource_snmp.go
@@ -207,6 +207,17 @@ func resourceSnmpReadWJnprSess(d *schema.ResourceData, m interface{}, jnprSess *
 func resourceSnmpUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	d.Partial(true)
 	sess := m.(*Session)
+	if sess.junosFakeUpdateAlso {
+		if err := delSnmp(m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		if err := setSnmp(d, m, nil); err != nil {
+			return diag.FromErr(err)
+		}
+		d.Partial(false)
+
+		return nil
+	}
 	jnprSess, err := sess.startNewSession()
 	if err != nil {
 		return diag.FromErr(err)
@@ -239,6 +250,13 @@ func resourceSnmpUpdate(ctx context.Context, d *schema.ResourceData, m interface
 func resourceSnmpDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	if d.Get("clean_on_destroy").(bool) {
 		sess := m.(*Session)
+		if sess.junosFakeDeleteAlso {
+			if err := delSnmp(m, nil); err != nil {
+				return diag.FromErr(err)
+			}
+
+			return nil
+		}
 		jnprSess, err := sess.startNewSession()
 		if err != nil {
 			return diag.FromErr(err)
