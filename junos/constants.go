093549a1c7de33a11294bf700a68748578581d31
Jeremy Muriel <jeremy@jeremm.fr>
2022-03-15 09:34:24 +0100

    reorder constants in file

--- junos/constants.go
+++ junos/constants.go
@@ -1,20 +1,24 @@
 package junos
 
 const (
-	idSeparator              = "_-_"
-	defaultWord              = "default"
-	evpnWord                 = "evpn"
-	inetWord                 = "inet"
-	inet6Word                = "inet6"
-	mplsWord                 = "mpls"
-	emptyWord                = "empty"
-	permitWord               = "permit"
-	deleteWord               = "delete"
-	setWord                  = "set"
-	setLineStart             = setWord + " "
-	st0Word                  = "st0"
-	ospfV2                   = "ospf"
-	ospfV3                   = "ospf3"
-	discardW                 = "discard"
-	disableW                 = "disable"
+	idSeparator = "_-_"
+	defaultWord = "default"
+
+	setWord      = "set"
+	setLineStart = setWord + " "
+	deleteWord   = "delete"
+
+	emptyWord  = "empty"
+	permitWord = "permit"
+	discardW   = "discard"
+	disableW   = "disable"
+
+	st0Word = "st0"
+
+	evpnWord  = "evpn"
+	inetWord  = "inet"
+	inet6Word = "inet6"
+	mplsWord  = "mpls"
+	ospfV2    = "ospf"
+	ospfV3    = "ospf3"
 )
