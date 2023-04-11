2a749bd55e5d25ab917bf2d46bda4c3baa71f215
Jeremy Muriel <jeremy@jeremm.fr>
2023-04-11 18:20:57 +0200

    tfvalidator: add unit tests for StringRegex validator

---
+++ internal/tfvalidator/string_regex_test.go
@@ -0,0 +1,61 @@
+package tfvalidator_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/jeremmfr/terraform-provider-junos/internal/tfvalidator"
+)
+
+func TestStringRegex(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		val         types.String
+		expectError bool
+	}
+	tests := map[string]testCase{
+		"unknown": {
+			val:         types.StringUnknown(),
+			expectError: false,
+		},
+		"null": {
+			val:         types.StringNull(),
+			expectError: false,
+		},
+		"valid": {
+			val:         types.StringValue("^.*$"),
+			expectError: false,
+		},
+		"invalid": {
+			val:         types.StringValue("^(.*$"),
+			expectError: true,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			request := validator.StringRequest{
+				Path:           path.Root("test"),
+				PathExpression: path.MatchRoot("test"),
+				ConfigValue:    test.val,
+			}
+			response := validator.StringResponse{}
+
+			tfvalidator.StringRegex().ValidateString(context.TODO(), request, &response)
+
+			if !response.Diagnostics.HasError() && test.expectError {
+				t.Fatal("expected error, got no error")
+			}
+
+			if response.Diagnostics.HasError() && !test.expectError {
+				t.Fatalf("got unexpected error: %s", response.Diagnostics)
+			}
+		})
+	}
+}
