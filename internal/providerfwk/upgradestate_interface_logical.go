856f312c4dba21a7935b706c3a8aa7bdbc84ecc9
Jeremy Muriel <jeremy@jeremm.fr>
2023-08-29 09:43:41 +0200

    r/interface_logical: upgradestate: avoid using block struct
    from the new version of resource schema when reading state with old version schema
    to prevent potential `Value Conversion Error` after adding an argument in block

--- internal/providerfwk/upgradestate_interface_logical.go
+++ internal/providerfwk/upgradestate_interface_logical.go
@@ -460,27 +460,128 @@ func upgradeInterfaceLogicalV0toV1(
 		SecurityZone             types.String   `tfsdk:"security_zone"`
 		VlanID                   types.Int64    `tfsdk:"vlan_id"`
 		FamilyInet               []struct {
-			SamplingInput  types.Bool                                    `tfsdk:"sampling_input"`
-			SamplingOutput types.Bool                                    `tfsdk:"sampling_output"`
-			FilterInput    types.String                                  `tfsdk:"filter_input"`
-			FilterOutput   types.String                                  `tfsdk:"filter_output"`
-			Mtu            types.Int64                                   `tfsdk:"mtu"`
-			Address        []interfaceLogicalBlockFamilyInetBlockAddress `tfsdk:"address"`
-			DHCP           []interfaceLogicalBlockFamilyInetBlockDhcp    `tfsdk:"dhcp"`
-			RPFCheck       []interfaceLogicalBlockFamilyBlockRPFCheck    `tfsdk:"rpf_check"`
+			SamplingInput  types.Bool   `tfsdk:"sampling_input"`
+			SamplingOutput types.Bool   `tfsdk:"sampling_output"`
+			FilterInput    types.String `tfsdk:"filter_input"`
+			FilterOutput   types.String `tfsdk:"filter_output"`
+			Mtu            types.Int64  `tfsdk:"mtu"`
+			Address        []struct {
+				Preferred types.Bool   `tfsdk:"preferred"`
+				Primary   types.Bool   `tfsdk:"primary"`
+				CidrIP    types.String `tfsdk:"cidr_ip"`
+				VRRPGroup []struct {
+					AcceptData              types.Bool     `tfsdk:"accept_data"`
+					NoAcceptData            types.Bool     `tfsdk:"no_accept_data"`
+					Preempt                 types.Bool     `tfsdk:"preempt"`
+					NoPreempt               types.Bool     `tfsdk:"no_preempt"`
+					Identifier              types.Int64    `tfsdk:"identifier"`
+					VirtualAddress          []types.String `tfsdk:"virtual_address"`
+					AdvertiseInterval       types.Int64    `tfsdk:"advertise_interval"`
+					AdvertisementsThreshold types.Int64    `tfsdk:"advertisements_threshold"`
+					AuthenticationKey       types.String   `tfsdk:"authentication_key"`
+					AuthenticationType      types.String   `tfsdk:"authentication_type"`
+					Priority                types.Int64    `tfsdk:"priority"`
+					TrackInterface          []struct {
+						Interface    types.String `tfsdk:"interface"`
+						PriorityCost types.Int64  `tfsdk:"priority_cost"`
+					} `tfsdk:"track_interface"`
+					TrackRoute []struct {
+						Route           types.String `tfsdk:"route"`
+						RoutingInstance types.String `tfsdk:"routing_instance"`
+						PriorityCost    types.Int64  `tfsdk:"priority_cost"`
+					} `tfsdk:"track_route"`
+				} `tfsdk:"vrrp_group"`
+			} `tfsdk:"address"`
+			DHCP []struct {
+				SrxOldOptionName                          types.Bool   `tfsdk:"srx_old_option_name"`
+				ClientIdentifierPrefixHostname            types.Bool   `tfsdk:"client_identifier_prefix_hostname"`
+				ClientIdentifierPrefixRoutingInstanceName types.Bool   `tfsdk:"client_identifier_prefix_routing_instance_name"`
+				ForceDiscover                             types.Bool   `tfsdk:"force_discover"`
+				LeaseTimeInfinite                         types.Bool   `tfsdk:"lease_time_infinite"`
+				NoDNSInstall                              types.Bool   `tfsdk:"no_dns_install"`
+				OptionsNoHostname                         types.Bool   `tfsdk:"options_no_hostname"`
+				UpdateServer                              types.Bool   `tfsdk:"update_server"`
+				ClientIdentifierASCII                     types.String `tfsdk:"client_identifier_ascii"`
+				ClientIdentifierHexadecimal               types.String `tfsdk:"client_identifier_hexadecimal"`
+				ClientIdentifierUseInterfaceDescription   types.String `tfsdk:"client_identifier_use_interface_description"`
+				ClientIdentifierUseridASCII               types.String `tfsdk:"client_identifier_userid_ascii"`
+				ClientIdentifierUseridHexadecimal         types.String `tfsdk:"client_identifier_userid_hexadecimal"`
+				LeaseTime                                 types.Int64  `tfsdk:"lease_time"`
+				Metric                                    types.Int64  `tfsdk:"metric"`
+				RetransmissionAttempt                     types.Int64  `tfsdk:"retransmission_attempt"`
+				RetransmissionInterval                    types.Int64  `tfsdk:"retransmission_interval"`
+				ServerAddress                             types.String `tfsdk:"server_address"`
+				VendorID                                  types.String `tfsdk:"vendor_id"`
+			} `tfsdk:"dhcp"`
+			RPFCheck []struct {
+				FailFilter types.String `tfsdk:"fail_filter"`
+				ModeLoose  types.Bool   `tfsdk:"mode_loose"`
+			} `tfsdk:"rpf_check"`
 		} `tfsdk:"family_inet"`
 		FamilyInet6 []struct {
-			DadDisable     types.Bool                                          `tfsdk:"dad_disable"`
-			SamplingInput  types.Bool                                          `tfsdk:"sampling_input"`
-			SamplingOutput types.Bool                                          `tfsdk:"sampling_output"`
-			FilterInput    types.String                                        `tfsdk:"filter_input"`
-			FilterOutput   types.String                                        `tfsdk:"filter_output"`
-			Mtu            types.Int64                                         `tfsdk:"mtu"`
-			Address        []interfaceLogicalBlockFamilyInet6BlockAddress      `tfsdk:"address"`
-			DHCPv6Client   []interfaceLogicalBlockFamilyInet6BlockDhcpV6Client `tfsdk:"dhcpv6_client"`
-			RPFCheck       []interfaceLogicalBlockFamilyBlockRPFCheck          `tfsdk:"rpf_check"`
+			DadDisable     types.Bool   `tfsdk:"dad_disable"`
+			SamplingInput  types.Bool   `tfsdk:"sampling_input"`
+			SamplingOutput types.Bool   `tfsdk:"sampling_output"`
+			FilterInput    types.String `tfsdk:"filter_input"`
+			FilterOutput   types.String `tfsdk:"filter_output"`
+			Mtu            types.Int64  `tfsdk:"mtu"`
+			Address        []struct {
+				Preferred types.Bool   `tfsdk:"preferred"`
+				Primary   types.Bool   `tfsdk:"primary"`
+				CidrIP    types.String `tfsdk:"cidr_ip"`
+				VRRPGroup []struct {
+					AcceptData              types.Bool     `tfsdk:"accept_data"`
+					NoAcceptData            types.Bool     `tfsdk:"no_accept_data"`
+					Preempt                 types.Bool     `tfsdk:"preempt"`
+					NoPreempt               types.Bool     `tfsdk:"no_preempt"`
+					Identifier              types.Int64    `tfsdk:"identifier"`
+					VirtualAddress          []types.String `tfsdk:"virtual_address"`
+					VirutalLinkLocalAddress types.String   `tfsdk:"virtual_link_local_address"`
+					AdvertiseInterval       types.Int64    `tfsdk:"advertise_interval"`
+					AdvertisementsThreshold types.Int64    `tfsdk:"advertisements_threshold"`
+					Priority                types.Int64    `tfsdk:"priority"`
+					TrackInterface          []struct {
+						Interface    types.String `tfsdk:"interface"`
+						PriorityCost types.Int64  `tfsdk:"priority_cost"`
+					} `tfsdk:"track_interface"`
+					TrackRoute []struct {
+						Route           types.String `tfsdk:"route"`
+						RoutingInstance types.String `tfsdk:"routing_instance"`
+						PriorityCost    types.Int64  `tfsdk:"priority_cost"`
+					} `tfsdk:"track_route"`
+				} `tfsdk:"vrrp_group"`
+			} `tfsdk:"address"`
+			DHCPv6Client []struct {
+				ClientIATypeNA                        types.Bool     `tfsdk:"client_ia_type_na"`
+				ClientIATypePD                        types.Bool     `tfsdk:"client_ia_type_pd"`
+				NoDNSInstall                          types.Bool     `tfsdk:"no_dns_install"`
+				RapidCommit                           types.Bool     `tfsdk:"rapid_commit"`
+				ClientIdentifierDuidType              types.String   `tfsdk:"client_identifier_duid_type"`
+				ClientType                            types.String   `tfsdk:"client_type"`
+				PrefixDelegatingPreferredPrefixLength types.Int64    `tfsdk:"prefix_delegating_preferred_prefix_length"`
+				PrefixDelegatingSubPrefixLength       types.Int64    `tfsdk:"prefix_delegating_sub_prefix_length"`
+				ReqOption                             []types.String `tfsdk:"req_option"`
+				RetransmissionAttempt                 types.Int64    `tfsdk:"retransmission_attempt"`
+				UpdateRouterAdvertisementInterface    []types.String `tfsdk:"update_router_advertisement_interface"`
+				UpdateServer                          types.Bool     `tfsdk:"update_server"`
+			} `tfsdk:"dhcpv6_client"`
+			RPFCheck []struct {
+				FailFilter types.String `tfsdk:"fail_filter"`
+				ModeLoose  types.Bool   `tfsdk:"mode_loose"`
+			} `tfsdk:"rpf_check"`
 		} `tfsdk:"family_inet6"`
-		Tunnel []interfaceLogicalBlockTunnel `tfsdk:"tunnel"`
+		Tunnel []struct {
+			AllowFragmentation         types.Bool   `tfsdk:"allow_fragmentation"`
+			DoNotFragment              types.Bool   `tfsdk:"do_not_fragment"`
+			PathMtuDiscovery           types.Bool   `tfsdk:"path_mtu_discovery"`
+			NoPathMtuDiscovery         types.Bool   `tfsdk:"no_path_mtu_discovery"`
+			Destination                types.String `tfsdk:"destination"`
+			Source                     types.String `tfsdk:"source"`
+			FlowLabel                  types.Int64  `tfsdk:"flow_label"`
+			RoutingInstanceDestination types.String `tfsdk:"routing_instance_destination"`
+			TrafficClass               types.Int64  `tfsdk:"traffic_class"`
+			TTL                        types.Int64  `tfsdk:"ttl"`
+		} `tfsdk:"tunnel"`
 	}
 
 	var dataV0 modelV0
@@ -509,39 +610,168 @@ func upgradeInterfaceLogicalV0toV1(
 	}
 	if len(dataV0.FamilyInet) > 0 {
 		dataV1.FamilyInet = &interfaceLogicalBlockFamilyInet{
+			SamplingInput:  dataV0.FamilyInet[0].SamplingInput,
+			SamplingOutput: dataV0.FamilyInet[0].SamplingOutput,
 			FilterInput:    dataV0.FamilyInet[0].FilterInput,
 			FilterOutput:   dataV0.FamilyInet[0].FilterOutput,
 			Mtu:            dataV0.FamilyInet[0].Mtu,
-			SamplingInput:  dataV0.FamilyInet[0].SamplingInput,
-			SamplingOutput: dataV0.FamilyInet[0].SamplingOutput,
-			Address:        dataV0.FamilyInet[0].Address,
+		}
+		for _, blockV0 := range dataV0.FamilyInet[0].Address {
+			blockV1 := interfaceLogicalBlockFamilyInetBlockAddress{
+				Preferred: blockV0.Preferred,
+				Primary:   blockV0.Primary,
+				CidrIP:    blockV0.CidrIP,
+			}
+			for _, subBlockV0 := range blockV0.VRRPGroup {
+				subBlockV1 := interfaceLogicalBlockFamilyInetBlockAddressBlockVRRPGroup{
+					AcceptData:              subBlockV0.AcceptData,
+					NoAcceptData:            subBlockV0.NoAcceptData,
+					Preempt:                 subBlockV0.Preempt,
+					NoPreempt:               subBlockV0.NoPreempt,
+					Identifier:              subBlockV0.Identifier,
+					VirtualAddress:          subBlockV0.VirtualAddress,
+					AdvertiseInterval:       subBlockV0.AdvertiseInterval,
+					AdvertisementsThreshold: subBlockV0.AdvertisementsThreshold,
+					AuthenticationKey:       subBlockV0.AuthenticationKey,
+					AuthenticationType:      subBlockV0.AuthenticationType,
+					Priority:                subBlockV0.Priority,
+				}
+				for _, subSubBlockV0 := range subBlockV0.TrackInterface {
+					subBlockV1.TrackInterface = append(subBlockV1.TrackInterface,
+						interfaceLogicalBlockFamilyBlockAddressBlockVRRPGroupBlockTrackInterface{
+							Interface:    subSubBlockV0.Interface,
+							PriorityCost: subSubBlockV0.PriorityCost,
+						},
+					)
+				}
+				for _, subSubBlockV0 := range subBlockV0.TrackRoute {
+					subBlockV1.TrackRoute = append(subBlockV1.TrackRoute,
+						interfaceLogicalBlockFamilyBlockAddressBlockVRRPGroupBlockTrackRoute{
+							Route:           subSubBlockV0.Route,
+							RoutingInstance: subSubBlockV0.RoutingInstance,
+							PriorityCost:    subSubBlockV0.PriorityCost,
+						},
+					)
+				}
+				blockV1.VRRPGroup = append(blockV1.VRRPGroup, subBlockV1)
+			}
+			dataV1.FamilyInet.Address = append(dataV1.FamilyInet.Address, blockV1)
 		}
 		if len(dataV0.FamilyInet[0].DHCP) > 0 {
-			dataV1.FamilyInet.DHCP = &dataV0.FamilyInet[0].DHCP[0]
+			dataV1.FamilyInet.DHCP = &interfaceLogicalBlockFamilyInetBlockDhcp{
+				SrxOldOptionName:                          dataV0.FamilyInet[0].DHCP[0].SrxOldOptionName,
+				ClientIdentifierPrefixHostname:            dataV0.FamilyInet[0].DHCP[0].ClientIdentifierPrefixHostname,
+				ClientIdentifierPrefixRoutingInstanceName: dataV0.FamilyInet[0].DHCP[0].ClientIdentifierPrefixRoutingInstanceName,
+				ForceDiscover:                             dataV0.FamilyInet[0].DHCP[0].ForceDiscover,
+				LeaseTimeInfinite:                         dataV0.FamilyInet[0].DHCP[0].LeaseTimeInfinite,
+				NoDNSInstall:                              dataV0.FamilyInet[0].DHCP[0].NoDNSInstall,
+				OptionsNoHostname:                         dataV0.FamilyInet[0].DHCP[0].OptionsNoHostname,
+				UpdateServer:                              dataV0.FamilyInet[0].DHCP[0].UpdateServer,
+				ClientIdentifierASCII:                     dataV0.FamilyInet[0].DHCP[0].ClientIdentifierASCII,
+				ClientIdentifierHexadecimal:               dataV0.FamilyInet[0].DHCP[0].ClientIdentifierHexadecimal,
+				ClientIdentifierUseInterfaceDescription:   dataV0.FamilyInet[0].DHCP[0].ClientIdentifierUseInterfaceDescription,
+				ClientIdentifierUseridASCII:               dataV0.FamilyInet[0].DHCP[0].ClientIdentifierUseridASCII,
+				ClientIdentifierUseridHexadecimal:         dataV0.FamilyInet[0].DHCP[0].ClientIdentifierUseridHexadecimal,
+				LeaseTime:                                 dataV0.FamilyInet[0].DHCP[0].LeaseTime,
+				Metric:                                    dataV0.FamilyInet[0].DHCP[0].Metric,
+				RetransmissionAttempt:                     dataV0.FamilyInet[0].DHCP[0].RetransmissionAttempt,
+				RetransmissionInterval:                    dataV0.FamilyInet[0].DHCP[0].RetransmissionInterval,
+				ServerAddress:                             dataV0.FamilyInet[0].DHCP[0].ServerAddress,
+				VendorID:                                  dataV0.FamilyInet[0].DHCP[0].VendorID,
+			}
 		}
 		if len(dataV0.FamilyInet[0].RPFCheck) > 0 {
-			dataV1.FamilyInet.RPFCheck = &dataV0.FamilyInet[0].RPFCheck[0]
+			dataV1.FamilyInet.RPFCheck = &interfaceLogicalBlockFamilyBlockRPFCheck{
+				FailFilter: dataV0.FamilyInet[0].RPFCheck[0].FailFilter,
+				ModeLoose:  dataV0.FamilyInet[0].RPFCheck[0].ModeLoose,
+			}
 		}
 	}
 	if len(dataV0.FamilyInet6) > 0 {
 		dataV1.FamilyInet6 = &interfaceLogicalBlockFamilyInet6{
 			DadDisable:     dataV0.FamilyInet6[0].DadDisable,
+			SamplingInput:  dataV0.FamilyInet6[0].SamplingInput,
+			SamplingOutput: dataV0.FamilyInet6[0].SamplingOutput,
 			FilterInput:    dataV0.FamilyInet6[0].FilterInput,
 			FilterOutput:   dataV0.FamilyInet6[0].FilterOutput,
 			Mtu:            dataV0.FamilyInet6[0].Mtu,
-			SamplingInput:  dataV0.FamilyInet6[0].SamplingInput,
-			SamplingOutput: dataV0.FamilyInet6[0].SamplingOutput,
-			Address:        dataV0.FamilyInet6[0].Address,
+		}
+		for _, blockV0 := range dataV0.FamilyInet6[0].Address {
+			blockV1 := interfaceLogicalBlockFamilyInet6BlockAddress{
+				Preferred: blockV0.Preferred,
+				Primary:   blockV0.Primary,
+				CidrIP:    blockV0.CidrIP,
+			}
+			for _, subBlockV0 := range blockV0.VRRPGroup {
+				subBlockV1 := interfaceLogicalBlockFamilyInet6BlockAddressBlockVRRPGroup{
+					AcceptData:              subBlockV0.AcceptData,
+					NoAcceptData:            subBlockV0.NoAcceptData,
+					Preempt:                 subBlockV0.Preempt,
+					NoPreempt:               subBlockV0.NoPreempt,
+					Identifier:              subBlockV0.Identifier,
+					VirtualAddress:          subBlockV0.VirtualAddress,
+					VirutalLinkLocalAddress: subBlockV0.VirutalLinkLocalAddress,
+					AdvertiseInterval:       subBlockV0.AdvertiseInterval,
+					AdvertisementsThreshold: subBlockV0.AdvertisementsThreshold,
+					Priority:                subBlockV0.Priority,
+				}
+				for _, subSubBlockV0 := range subBlockV0.TrackInterface {
+					subBlockV1.TrackInterface = append(subBlockV1.TrackInterface,
+						interfaceLogicalBlockFamilyBlockAddressBlockVRRPGroupBlockTrackInterface{
+							Interface:    subSubBlockV0.Interface,
+							PriorityCost: subSubBlockV0.PriorityCost,
+						},
+					)
+				}
+				for _, subSubBlockV0 := range subBlockV0.TrackRoute {
+					subBlockV1.TrackRoute = append(subBlockV1.TrackRoute,
+						interfaceLogicalBlockFamilyBlockAddressBlockVRRPGroupBlockTrackRoute{
+							Route:           subSubBlockV0.Route,
+							RoutingInstance: subSubBlockV0.RoutingInstance,
+							PriorityCost:    subSubBlockV0.PriorityCost,
+						},
+					)
+				}
+				blockV1.VRRPGroup = append(blockV1.VRRPGroup, subBlockV1)
+			}
+			dataV1.FamilyInet6.Address = append(dataV1.FamilyInet6.Address, blockV1)
 		}
 		if len(dataV0.FamilyInet6[0].DHCPv6Client) > 0 {
-			dataV1.FamilyInet6.DHCPv6Client = &dataV0.FamilyInet6[0].DHCPv6Client[0]
+			dataV1.FamilyInet6.DHCPv6Client = &interfaceLogicalBlockFamilyInet6BlockDhcpV6Client{
+				ClientIATypeNA:                        dataV0.FamilyInet6[0].DHCPv6Client[0].ClientIATypeNA,
+				ClientIATypePD:                        dataV0.FamilyInet6[0].DHCPv6Client[0].ClientIATypePD,
+				NoDNSInstall:                          dataV0.FamilyInet6[0].DHCPv6Client[0].NoDNSInstall,
+				RapidCommit:                           dataV0.FamilyInet6[0].DHCPv6Client[0].RapidCommit,
+				ClientIdentifierDuidType:              dataV0.FamilyInet6[0].DHCPv6Client[0].ClientIdentifierDuidType,
+				ClientType:                            dataV0.FamilyInet6[0].DHCPv6Client[0].ClientType,
+				PrefixDelegatingPreferredPrefixLength: dataV0.FamilyInet6[0].DHCPv6Client[0].PrefixDelegatingPreferredPrefixLength,
+				PrefixDelegatingSubPrefixLength:       dataV0.FamilyInet6[0].DHCPv6Client[0].PrefixDelegatingSubPrefixLength,
+				ReqOption:                             dataV0.FamilyInet6[0].DHCPv6Client[0].ReqOption,
+				RetransmissionAttempt:                 dataV0.FamilyInet6[0].DHCPv6Client[0].RetransmissionAttempt,
+				UpdateRouterAdvertisementInterface:    dataV0.FamilyInet6[0].DHCPv6Client[0].UpdateRouterAdvertisementInterface,
+				UpdateServer:                          dataV0.FamilyInet6[0].DHCPv6Client[0].UpdateServer,
+			}
 		}
 		if len(dataV0.FamilyInet6[0].RPFCheck) > 0 {
-			dataV1.FamilyInet6.RPFCheck = &dataV0.FamilyInet6[0].RPFCheck[0]
+			dataV1.FamilyInet6.RPFCheck = &interfaceLogicalBlockFamilyBlockRPFCheck{
+				FailFilter: dataV0.FamilyInet6[0].RPFCheck[0].FailFilter,
+				ModeLoose:  dataV0.FamilyInet6[0].RPFCheck[0].ModeLoose,
+			}
 		}
 	}
 	if len(dataV0.Tunnel) > 0 {
-		dataV1.Tunnel = &dataV0.Tunnel[0]
+		dataV1.Tunnel = &interfaceLogicalBlockTunnel{
+			AllowFragmentation:         dataV0.Tunnel[0].AllowFragmentation,
+			DoNotFragment:              dataV0.Tunnel[0].DoNotFragment,
+			PathMtuDiscovery:           dataV0.Tunnel[0].PathMtuDiscovery,
+			NoPathMtuDiscovery:         dataV0.Tunnel[0].NoPathMtuDiscovery,
+			Destination:                dataV0.Tunnel[0].Destination,
+			Source:                     dataV0.Tunnel[0].Source,
+			FlowLabel:                  dataV0.Tunnel[0].FlowLabel,
+			RoutingInstanceDestination: dataV0.Tunnel[0].RoutingInstanceDestination,
+			TrafficClass:               dataV0.Tunnel[0].TrafficClass,
+			TTL:                        dataV0.Tunnel[0].TTL,
+		}
 	}
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, dataV1)...)
