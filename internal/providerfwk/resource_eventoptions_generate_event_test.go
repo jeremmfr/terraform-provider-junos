9e72755da309b82b6af5cceea12f31b10eb7e0aa
Jeremy Muriel <jeremy@jeremm.fr>
2024-03-12 20:08:34 +0100

    r/eventoptions_generate_event: testacc: disable tests with switch
    start_time not compatible

--- internal/providerfwk/resource_eventoptions_generate_event_test.go
+++ internal/providerfwk/resource_eventoptions_generate_event_test.go
@@ -1,6 +1,7 @@
 package providerfwk_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
@@ -8,21 +9,23 @@ import (
 )
 
 func TestAccResourceEventoptionsGenerateEvent_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				ConfigDirectory: config.TestStepDirectory(),
+	if os.Getenv("TESTACC_SWITCH") == "" {
+		resource.Test(t, resource.TestCase{
+			PreCheck:                 func() { testAccPreCheck(t) },
+			ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					ResourceName:      "junos_eventoptions_generate_event.testacc_evtopts_genevent",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
 			},
-			{
-				ConfigDirectory: config.TestStepDirectory(),
-			},
-			{
-				ResourceName:      "junos_eventoptions_generate_event.testacc_evtopts_genevent",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
+		})
+	}
 }
