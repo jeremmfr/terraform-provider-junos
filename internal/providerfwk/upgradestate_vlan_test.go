45026bcd91ffff8c154aa71f357d5ffc960887bc
Jeremy Muriel <jeremy@jeremm.fr>
2024-04-30 13:48:52 +0200

    r/vlan: tests: fix Junos device type to test state upgrade

--- internal/providerfwk/upgradestate_vlan_test.go
+++ internal/providerfwk/upgradestate_vlan_test.go
@@ -1,6 +1,7 @@
 package providerfwk_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
@@ -10,20 +11,22 @@ import (
 
 // export TESTACC_INTERFACE=<inteface> for choose interface available else it's ge-0/0/3.
 func TestAccUpgradeStateResourceVlan_V0toV1_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		Steps: []resource.TestStep{
-			{
-				ConfigDirectory: config.TestStepDirectory(),
-			},
-			{
-				ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
-				ConfigDirectory:          config.TestStepDirectory(),
-				ConfigPlanChecks: resource.ConfigPlanChecks{
-					PreApply: []plancheck.PlanCheck{
-						plancheck.ExpectEmptyPlan(),
+	if os.Getenv("TESTACC_SWITCH") != "" {
+		resource.Test(t, resource.TestCase{
+			Steps: []resource.TestStep{
+				{
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+					ConfigDirectory:          config.TestStepDirectory(),
+					ConfigPlanChecks: resource.ConfigPlanChecks{
+						PreApply: []plancheck.PlanCheck{
+							plancheck.ExpectEmptyPlan(),
+						},
 					},
 				},
 			},
-		},
-	})
+		})
+	}
 }
