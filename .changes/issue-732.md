f38f079ea0847d012de5943628d64e1acf45386d
Jeremy Muriel <jeremy@jeremm.fr>
2025-01-02 11:20:10 +0100

    add junos_mstp resource
    partial fix #732

---
+++ .changes/issue-732.md
@@ -0,0 +1,4 @@
+<!-- markdownlint-disable-file MD013 MD041 -->
+FEATURES:
+
+* add **junos_mstp** resource (Partial fix [#732](https://github.com/jeremmfr/terraform-provider-junos/issues/732))
--- internal/providerfwk/provider.go
+++ internal/providerfwk/provider.go
@@ -264,6 +264,7 @@ func (p *junosProvider) Resources(_ context.Context) []func() resource.Resource 
 		newInterfacePhysicalDisableResource,
 		newInterfacePhysicalResource,
 		newInterfaceSt0UnitResource,
+		newMstpResource,
 		newMultichassisResource,
 		newMultichassisProtectionPeerResource,
 		newOamGretunnelInterfaceResource,
