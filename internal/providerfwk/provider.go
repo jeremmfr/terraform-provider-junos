5623c26cacd0ee82271564ede15c6bfd00c01786
Jeremy Muriel <jeremy@jeremm.fr>
2025-01-06 09:12:06 +0100

    r/system_login_class: use new provider via framework

--- internal/providerfwk/provider.go
+++ internal/providerfwk/provider.go
@@ -323,6 +323,7 @@ func (p *junosProvider) Resources(_ context.Context) []func() resource.Resource 
 		newSnmpViewResource,
 		newStaticRouteResource,
 		newSwitchOptionsResource,
+		newSystemLoginClassResource,
 		newSystemLoginUserResource,
 		newSystemResource,
 		newSystemRadiusServerResource,
