0b1223b544796c870e769f0d75caaaedc30c1a79
Jeremy Muriel <jeremy@jeremm.fr>
2025-12-13 17:47:26 +0100

    r/apply_group*,r/group_raw: fix tests to only with SRX devices

--- internal/provider/resource_apply_group_except_test.go
+++ internal/provider/resource_apply_group_except_test.go
@@ -1,6 +1,7 @@
 package provider_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
@@ -8,24 +9,26 @@ import (
 )
 
 func TestAccResourceApplyGroupExcept_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				// 1
-				ConfigDirectory: config.TestStepDirectory(),
+	if os.Getenv("TESTACC_SRX") != "" {
+		resource.Test(t, resource.TestCase{
+			PreCheck:                 func() { testAccPreCheck(t) },
+			ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					// 1
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					// 2
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					// 3
+					ResourceName:      "junos_apply_group_except.testacc_foobar",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
 			},
-			{
-				// 2
-				ConfigDirectory: config.TestStepDirectory(),
-			},
-			{
-				// 3
-				ResourceName:      "junos_apply_group_except.testacc_foobar",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
+		})
+	}
 }
--- internal/provider/resource_apply_group_test.go
+++ internal/provider/resource_apply_group_test.go
@@ -1,6 +1,7 @@
 package provider_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/config"
@@ -8,36 +9,38 @@ import (
 )
 
 func TestAccResourceApplyGroup_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				// 1
-				ConfigDirectory: config.TestStepDirectory(),
+	if os.Getenv("TESTACC_SRX") != "" {
+		resource.Test(t, resource.TestCase{
+			PreCheck:                 func() { testAccPreCheck(t) },
+			ProtoV5ProviderFactories: testAccProtoV5ProviderFactories,
+			Steps: []resource.TestStep{
+				{
+					// 1
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					// 2
+					ConfigDirectory: config.TestStepDirectory(),
+				},
+				{
+					// 3
+					ResourceName:      "junos_apply_group.testacc_foobar",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
+				{
+					// 4
+					ResourceName:      "junos_apply_group.testacc_barfoo",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
+				{
+					// 5
+					ResourceName:      "junos_apply_group.testacc_barfoo2",
+					ImportState:       true,
+					ImportStateVerify: true,
+				},
 			},
-			{
-				// 2
-				ConfigDirectory: config.TestStepDirectory(),
-			},
-			{
-				// 3
-				ResourceName:      "junos_apply_group.testacc_foobar",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				// 4
-				ResourceName:      "junos_apply_group.testacc_barfoo",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-			{
-				// 5
-				ResourceName:      "junos_apply_group.testacc_barfoo2",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
+		})
+	}
 }
